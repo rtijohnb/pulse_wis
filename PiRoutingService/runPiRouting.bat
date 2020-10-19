da2aa1bed185867c91cca2b79b7f09f1ae592668
Ken McInerney <kenm@rti.com>
2020-10-19 15:19:32 -0400

    Added some "start" scripts to make it easier for others to figure out how to start the routing pieces.

---
+++ PiRoutingService/runPiRouting.bat
@@ -0,0 +1,4 @@
+@echo off
+"%NDDSHOME%/bin/rtiroutingservice.bat" ^
+  -cfgFile pi_server_routing_config.xml ^
+  -cfgName PulsePi ^
---
+++ runWisRouting.bat
@@ -0,0 +1,4 @@
+@echo off
+"%NDDSHOME%/bin/rtiroutingservice.bat" ^
+  -cfgFile WIS_server_routing_config.xml ^
+  -cfgName PulseWis ^
