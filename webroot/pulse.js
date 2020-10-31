2ff0c386940b892fd998be062dd751b624897c49
Paul Schmitt <42389607+psmass@users.noreply.github.com>
2020-10-31 08:00:20 -0400

    Manual  sync of main with AndyPulse Branch

--- webroot/pulse.js
+++ webroot/pulse.js
@@ -16,17 +16,31 @@ var rti = rti || {};
  * @namespace rti.pulse
  */
 rti.pulseapp = {
-    X_POINT_COUNT: 500,
-    patientId: null,
+    X_POINT_COUNT: 1000,
+    BROWSER_UPDATE_RATE_MS: 30,
+    patientId: "pid123",
+    patientConfig: {
+        high: 90, 
+        low: 50,
+    },
     chartConfig: {},
-    updateCount: 0,
-    emptyCount: 0,
+    counts: {
+        updateCount: 0, emptyCount: 0,
+        sampleCount: 0, totalSampleCount: 0,
+    },
     lineChart: null,
     prevSampleTimestamp: -1, /* keep track of previous sample ts to filter out dups now that we read() vs. take() */
-    bumpEmptyCount: function() { this.emptyCount++; return this.emptyCount; },
-    getUpdateCount: function() { this.updateCount++; return this.updateCount; },
+
+    bumpEmptyCount:  function() { this.counts.emptyCount++;  return this.counts.emptyCount; },
+    bumpUpdateCount: function() { this.counts.updateCount++; return this.counts.updateCount; },
+    getUpdateCount:  function() { return this.counts.updateCount; },
+    getSampleCount:  function() { return this.counts.sampleCount; },
+    getTotalSampleCount: function() { return this.counts.totalSampleCount; },
+    setSampleCount: function(n) { this.counts.sampleCount = n; this.counts.totalSampleCount += n; return this.counts.sampleCount; },
+
     getPatientId: function() { return this.patientId;},
     setPatientId: function(id) { this.patientId = id; },
+
     getBaseURL: function(is_pub) {
       var app = "/dds/rest1/applications/PulseWisApp";
       var pant = "domain_participants/PulseWisParticipant";
@@ -39,13 +53,16 @@ rti.pulseapp = {
       return `${app}/${pant}/${pOrS}`;
     },
     getPulseReaderURL: function() {
-      return this.getBaseURL(false) + "/data_readers/PatientPulseReader";
+        return this.getBaseURL(false) + "/data_readers/PatientPulseReader";
+    },
+    getPatientConfigReaderURL: function() {
+    return this.getBaseURL(false) + "/data_readers/PatientConfigReader";
     },
     getPatientInfoReaderURL: function() {
-      return this.getBaseURL(false) + "/data_readers/PatientInfoReader";
+    return this.getBaseURL(false) + "/data_readers/PatientInfoReader";
     },
-    getConfigWriterURL: function() {
-      return this.getBaseURL(true) + "/data_writers/PatientConfigWriter";
+    getPatientConfigWriterURL: function() {
+    return this.getBaseURL(true) + "/data_writers/PatientConfigWriter";
     },
     /**
      * Sets up a new chart. This method needs to be called before reading or drawing ecg info.
@@ -63,6 +80,13 @@ rti.pulseapp = {
                     borderColor: 'rgb(255, 99, 132)',
                     data: [],
                     fill: false,
+                    pointRadius:1, // removes the dots, default: 3
+                }, {
+                    label: "bar",
+                    backgroundColor: 'rgba(200,200,200,1)',
+                    borderColor: 'rgba(200,200,200,1)',
+                    data: [],
+                    fill: true,
                     pointRadius:1, // removes the dots, default: 3
                 }],
             },
@@ -87,8 +111,8 @@ rti.pulseapp = {
 			            gridLines: { display: false},
  		                ticks: { display: false},
                         scaleLabel: {
-                            display: false,
-                            labelString: 'Some Patient',
+                            display: true,
+                            labelString: '', //'Some Patient',
                         }
                     }],
                     yAxes: [{
@@ -111,39 +135,93 @@ rti.pulseapp = {
             this.chartConfig.data.labels.push('0:00');
             this.chartConfig.data.datasets[0].data.push(500);
         };
+        //this.lineChart.update();
 
+        /* Button handlers  */
+        document.getElementById("btnHighUpId").addEventListener(
+            "click", 
+            function() { 
+                rti.pulseapp.writePatientConfig(rti.pulseapp.patientConfig.high+10, rti.pulseapp.patientConfig.low);
+            }, 
+            false
+        );
+        document.getElementById("btnHighDownId").addEventListener(
+            "click", 
+            function() { 
+                rti.pulseapp.writePatientConfig(rti.pulseapp.patientConfig.high-10, rti.pulseapp.patientConfig.low);
+            }, 
+            false
+        );
+        document.getElementById("btnLowUpId").addEventListener(
+            "click", 
+            function() { 
+                rti.pulseapp.writePatientConfig(rti.pulseapp.patientConfig.high, rti.pulseapp.patientConfig.low+10);
+                }, 
+            false
+         );
+        document.getElementById("btnLowDownId").addEventListener(
+            "click", 
+            function() { 
+                rti.pulseapp.writePatientConfig(rti.pulseapp.patientConfig.high, rti.pulseapp.patientConfig.low-10);
+            }, 
+            false
+        );
+        rti.pulseapp.initPatientInfo();
+        rti.pulseapp.initPatientConfig();  
+    },
+
+    initPatientInfo:function() {
         let url = this.getPatientInfoReaderURL();
         $.getJSON(
-          url,
-          { sampleFormat:"json"},
-          function(info) {
-            if (info && info.length) {
-	        rti.pulseapp.updatePatientInfo(info[0]);
-	    }
-        });
-
-	/* Button handlers 
-        document.getElementById("btn50").addEventListener("click", function() { rti.pulseapp.write(50, 1);}, false);
-        document.getElementById("btn100").addEventListener("click", function() { rti.pulseapp.write(100, 2);}, false);
-        document.getElementById("btn200").addEventListener("click", function() { rti.pulseapp.write(200, 3);}, false); 
-	*/
+            url,
+            { sampleFormat:"json", removeFromReaderCache: "false"}, 
+            function(info) {
+                if (info && info.length) {
+                    rti.pulseapp.updatePatientInfo(info[0]);
+                }
+            }
+        );
     },
-    updatePatientInfo(data) {
+
+    initPatientConfig:function() {
+    // first browser to connect will publish the default value
+        let url = this.getPatientConfigReaderURL();
+        
+        $.getJSON(
+            url,
+            { sampleFormat:"json", removeFromReaderCache: "false"}, 
+            function(samples) {
+                let data;
+                if (samples && samples.length) {
+                    data = samples[0].data;
+                } else {
+                    rti.pulseapp.writePatientConfig(rti.pulseapp.patientConfig.high, rti.pulseapp.patientConfig.low);
+                    data = {
+                        "PulseHighThreshold": rti.pulseapp.patientConfig.high,
+                        "PulseLowThreshold":  rti.pulseapp.patientConfig.low,
+                    };
+                }
+                rti.pulseapp.updatePatientConfig(data.PulseHighThreshold, data.PulseLowThreshold);
+            }
+        );
+        
+    }, 
+    
+    updatePatientInfo(sample) {
         // console.log(data);
-        const PATIENT_ITEM = document.getElementById('patientNameId');
-        let html_name = `${data.data.FirstName} ${data.data.LastName} &nbsp; &nbsp; Age: ${data.data.Age}`;
-        PATIENT_ITEM.innerHTML = html_name;
-        // remove name/age from chart canvas, showing in HTML
-	    //let name = `${data.data.FirstName} ${data.data.LastName}     Age: ${data.data.Age}`;
-	    //this.chartConfig.options.scales.xAxes[0].scaleLabel.labelString = name;
+        let name = `${sample.data.FirstName} ${sample.data.LastName}     Age: ${sample.data.Age}  ID: ${sample.data.Id.Id}`;
+        this.chartConfig.options.scales.xAxes[0].scaleLabel.labelString = name;
+            //console.log('updatePatientInfo: ' + sample.data.Id.Id);
+        this.patientId = sample.data.Id.Id;
     },
     /**
      *  The method will call the methods that update the display at 33 ms intervals.
      */
-    read: function() {
+    run: function() {
         var url = this.getPulseReaderURL();
+        const chartUpdateIntervalPeriod = rti.pulseapp.BROWSER_UPDATE_RATE_MS; // in milliseconds 2x data rate
 
-        var chartUpdateIntervalPeriod = 500; // in milliseconds 2x data rate
+        var configURL = this.getPatientConfigReaderURL();
 
         // Call chartjs() for ecgPulse and bpm every ecgReadIntervalPeriod, passing the data resulting
         // for reading new samples of the appropriate topic in json format without deleting the samples
@@ -152,21 +230,30 @@ rti.pulseapp = {
             // Read pulse
             $.getJSON(
                 url,
-                {
-                    sampleFormat: "json",
-                    removeFromReaderCache: "false"
-                },
+                { sampleFormat: "json", removeFromReaderCache: "false" },
                 function(data) {
-		    if (data) {
+                    if (data) {
                         rti.pulseapp.updateChart(data); 
-		    } else {
- 		        console.log('got empty data' + this.bumpEmptyCnt());
-		    }
+                    } else {
+                        console.log('got empty data' + this.bumpEmptyCnt());
+                    }
+                }
+            );
+            // Also update the PatientConfig value
+            $.getJSON(
+                configURL,
+                { sampleFormat:"json", removeFromReaderCache: "false" }, 
+                function(samples) {
+                    //console.log('config: ', samples);
+                    if (samples && samples.length) { 
+                        let data = samples[0].data;
+                        rti.pulseapp.updatePatientConfig(data.PulseHighThreshold, data.PulseLowThreshold);
+                    }
                 }
             );
         }, chartUpdateIntervalPeriod);
     },
-
+  
     /**
      * Updates the chart with the sequence of values
      * @param sampleSeq Sequence of samples to be drawn.
@@ -176,34 +263,35 @@ rti.pulseapp = {
         var chartData = this.chartConfig.data.datasets[0].data;
         var chartLabels = this.chartConfig.data.labels;
         var lineChart = this.lineChart;
-
-        const COUNT_ITEM = document.getElementById("countId");
-        COUNT_ITEM.innerHTML = "update count: " + rti.pulseapp.getUpdateCount();
         
         // how to change the line color in time (can also be changed by value condition)
         // lineChart.config.data.datasets[0].borderColor="rgb(255, 99, 132)";
         // lineChart.config.data.datasets[0].backgroundColor="rgb(255, 99, 132)";
     
-
+        // console.log ("Sample seq len: ", sampleSeq.length)
+        //if (sampleSeq.length == 100) {
         sampleSeq.forEach(function(sample, i, samples) {
             // Process metadata
             
             // console.log("sample", sample);
             // console.log(sample.data.readings.length);
 
-	        var info = sample.read_sample_info;
+            var info = sample.read_sample_info;
             var valid_data = info.valid_data;
             var instance_handle = info.instance_handle;
             var instance_state  = info.instance_state;
             var reception_time  = info.source_timestamp;
             //var averageReading;
+            rti.pulseapp.setSampleCount(sample.data.readings.length);
 
             //console.log("sample received:", reception_time);
             // If we received an invalid data sample, and the instance state
             // is != ALIVE, then the instance has been either disposed or
             // unregistered and we ignore the sample.
+            if (sample.data.timestamp > rti.pulseapp.prevSampleTimestamp+1) 
+                console.log("Timestamp Error: ", sample.data.timestamp, rti.pulseapp.prevSampleTimestamp);
             if (valid_data && (instance_state == "ALIVE") && 
-                (sample.data.timestamp != rti.pulseapp.prevSampleTimestamp)) {
+                (sample.data.timestamp > rti.pulseapp.prevSampleTimestamp)) {
                     rti.pulseapp.prevSampleTimestamp = sample.data.timestamp;
                     // console.log(sample.data.timestamp);
                     // console.log(chartData.length);
@@ -212,40 +300,66 @@ rti.pulseapp = {
 
                     sample.data.readings.forEach(function(reading, index){
                         //index += index;
-                        if (reading < 100) { return; } //- return from this sample - simple filter to get rid of spikes in the data
+                        // if (reading < 100) { return; } //- return from this sample - simple filter to get rid of spikes in the data
                         // Better to fix this at the source (not the browser) or integrate the sample more.
 
                         chartLabels.shift();
                         chartData.shift();
 
-			            //console.log(reception_time.sec);
-		                let dt = new Date (reception_time.sec * 1000);
+                        //console.log(reception_time.sec);
+                        let dt = new Date (reception_time.sec * 1000);
                         chartLabels.push(('00'+dt.getMinutes()).slice(-2) + ':' + 
-				         ('00'+dt.getSeconds()).slice(-2));
+                                        ('00'+dt.getSeconds()).slice(-2));
                         chartData.push(reading);
                     });
-                    lineChart.update();
-
+                    //lineChart.update();
                     var value = sample.data.bpm;
                     var elementHb = document.getElementById("heartbeatValue");
                     elementHb.innerHTML = value;
 
+            } else {  // we re-read prior sequential set of data from cache - we don't need to re-display (only a waste of time)
+                // console.log("Dropping,Read old data: prev ts " + rti.pulseapp.prevSampleTimestamp + "data ts " + sample.data.timestamp + " rec: " + info.source_timestamp.sec);
             }
+            const COUNT_ITEM = document.getElementById("countId");
+            COUNT_ITEM.innerHTML = "update count: " + rti.pulseapp.bumpUpdateCount() +
+                    " sample count: " + rti.pulseapp.getSampleCount() +
+                    " total samples: " + rti.pulseapp.getTotalSampleCount();
         });
+        lineChart.update();
+        //}
     },
+    /* update the screen's config */
+    updatePatientConfig: function(high, low) {
+        //console.log({updatePatientConfig:{high: high, low: low}});
+        rti.pulseapp.patientConfig.high = high;
+        rti.pulseapp.patientConfig.low = low;
+        $("#highValueId").prop("innerHTML", high);
+        $("#lowValueId").prop("innerHTML", low);
 
-	/* Write some value back on the PatientConfig topic
-	 * INACTIVE
-    write: function(highValue, lowValue) {
-      var configURL = this.getConfigWriterURL();
+        if (high >= 200)
+         $('#btnHighUpId').addClass('disabled');
+        else
+         $('#btnHighUpId').removeClass('disabled');
+
+        $('#btnHighUpId').prop("disabled", high >= 200);
+        $('#btnHighDownId').prop("disabled", high <= 0);
+        $('#btnLowUpId').prop("disabled", low >= 200);
+        //$('#btnLowDownId').prop("disabled", low <= 10);
+        document.getElementById('btnLowDownId').disabled = (low <= 0);
+        //let x = document.getElementById('btnLowDownId').disabled;
+        //console.log({disabled: x});
+    },
+	/* Write some value back on the PatientConfig topic */
+    writePatientConfig: function(highValue, lowValue) {
+        const configURL = this.getPatientConfigWriterURL();
         var configData = { 
-	  "Id": { "Id": "iamGroot", },
-	  "PulseHighThreshold": highValue,
-	  "PulseLowThreshold": lowValue,
-	};
+            Id: { Id: this.patientId.toString() }, 
+            PulseHighThreshold: highValue,
+            PulseLowThreshold: lowValue,
+        };
 
-	var configDataJSON = JSON.stringify(configData);
-	console.log(configDataJSON);
+        var configDataJSON = JSON.stringify(configData);
+        // console.log(configDataJSON);
 
         $.ajax({
           url:configURL,
@@ -254,9 +368,9 @@ rti.pulseapp = {
           contentType:"application/dds-web+json",
           dataType:"json",
           success: function(param){
-            console.log("sent " + configDataJSON);
+            //console.log("sent " + configDataJSON);
 		 
           }
         });
-    }, */ 
+    }, 
 }
