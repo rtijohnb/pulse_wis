4b08427884f973826191254a7fed83624b6294f1
Ken McInerney <kenm@rti.com>
2020-11-25 15:32:03 -0500

    Integrating some of my recent changes and migrating to using the MedicalDemo.xml file - for now types are pulled from there.  QoS will move over there next.

--- runPulseWis.bat
+++ runPulseWis.bat
@@ -1,4 +1,5 @@
 @echo off
+set NDDS_QOS_PROFILES=file://C:/users/kenm/repos/Pulse/pulse_sysdesigner/MedicalDemo.xml
 set WIS_HTTP_PORT=8090
 echo Listening on %WIS_HTTP_PORT%
 "%NDDSHOME%/bin/rtiwebintegrationservice.bat" ^
